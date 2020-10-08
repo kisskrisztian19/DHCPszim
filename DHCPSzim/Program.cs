97b070116ffffed8fc353d8ad24b107552e40cad
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-10-08 13:15:58 +0200

    Órák vége, request parancs működése, címek kiosztása a feltételek szerint

--- DHCPSzim/Program.cs
+++ DHCPSzim/Program.cs
@@ -61,6 +61,64 @@ namespace DHCPSzim
             return $"{ip[0]}.{ip[1]}.{ip[2]}.{ip[3]}";
         }
 
+        static void Feladat(string parancs)
+        {
+            /*
+             * parancs = "request;MAC-cím"
+             * először csak "request" paranccsal foglalkozunk
+             * 
+             * Megnézzük, hogy "request"-e (x)
+             * Ki kell szedni a MAC címet a parancsból
+             */
+            if (parancs.Contains("request"))
+            {
+                string[] a = parancs.Split(';');
+                string mac = a[1];
+                if (dhcp.ContainsKey(mac))
+                {
+                    Console.WriteLine($"DHCP: {mac} -> {dhcp[mac]}"); //A dhcp-nek nem értéket adunk, hanem kulcsot, amihez kiadja a value-t
+                }
+                else
+                {
+                    if (reserved.ContainsKey(mac))
+                    {
+                        Console.WriteLine($"RESERVED: {mac} -> {reserved[mac]}");
+                        dhcp.Add(mac, reserved[mac]);
+                    }
+                    else
+                    {
+                        string indulo = "192.168.10.100";
+                        int okt4 = 100;
+
+                        while (okt4 < 200 && (dhcp.ContainsValue(indulo) || reserved.ContainsValue(indulo) || excluded.Contains(indulo)))
+                        {
+                            okt4++;
+                            indulo = CimEggyelNo(indulo);
+                        }
+                        if (okt4 < 200)
+                        {
+                            Console.WriteLine($"Kiosztott: {mac} -> {indulo}");
+                            dhcp.Add(mac, indulo);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{mac} nincs IP!");
+                        }
+                    }
+                }
+            }
+            else Console.WriteLine("Nem oké");
+
+        }
+
+        static void Feladatok()
+        {
+            foreach (var command in commands)
+            {
+                Feladat(command);
+            }
+        }
+
         static void BeolvasDictionary(Dictionary<string, string> d, string filenev)
         {
             try
@@ -84,10 +142,7 @@ namespace DHCPSzim
             BeolvasList(commands, "test.csv");
             BeolvasDictionary(dhcp, "dhcp.csv");
             BeolvasDictionary(reserved, "reserved.csv");
-            foreach (var d in commands)
-            {
-                Console.WriteLine(d);
-            }
+            Feladatok();
             Console.WriteLine("\nVége...");
             Console.ReadLine();
         }
